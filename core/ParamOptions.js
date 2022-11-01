096606058f137ab9a5463a9b92ccd1387376a270
Chadwick Wood <chadwick@coffeeshopped.com>
2022-11-01 15:44:16 -0500

    A bunch of refactoring to remove all the 'yield' madness

--- core/ParamOptions.js
+++ core/ParamOptions.js
@@ -1,85 +1,88 @@
+require('/core/ArrayUtils.js')
 
-class ParamOptions {
-  
-  static reduce(genFn, itemMapFn = ((item) => item)) {
-    // each call to generator should yield an array of arrays of [path, optionsObject]
-    let opts = []
-    for(let arr of genFn()) {
-      opts.push(...arr.map(itemMapFn));
-    }
-    return opts
-  }
-    
-  static prefix = function(pfx, {count = null, bx = 0, px = null} = {}, genFn) {
-    const self = ParamOptions
-    if (count === null) {
-      return self.reduce(genFn, (item) => [pfx.concat(item[0]), item[1]])
-    }
-    else {
-      return self.reduce(function*() {
-        for (let i=0; i<count; ++i) {
-          let p = px === null ? null : px * i
-          yield self.prefix(pfx.concat([i]), {}, function*() {
-            yield self.offset({ b: bx * i, p: p }, genFn)
-          })
-        }
-      })
-    }
+function reduce(genFn, itemMapFn = ((item) => item)) {
+  // genFn can also just be an array, in which case, return it.
+  if (Array.isArray(genFn)) {
+    return genFn.mapWithIndex(itemMapFn)
   }
   
-    static prefixes(pfxs, {bx = 0, px = null} = {}, genFn) {
-      const self = ParamOptions
-      let opts = []
-      for (let i=0; i<pfxs.length; ++i) {
-        const pfx = pfxs[i]
-        let p = px == null ? null : px * i
-        let item = self.prefix(pfx, {}, function*() {
-          yield self.offset({ b: bx * i, p: p }, genFn)
-        })
-        opts = opts.concat(item)
+  // each call to generator should yield an array of arrays of [path, optionsObject]  
+  let opts = []
+  for(let arr of genFn()) {
+    opts.push(...arr.mapWithIndex(itemMapFn));
+  }
+  return opts
+}
+  
+function prefix(pfx, {count = null, bx = 0, px = null} = {}, genFn) {
+  if (count === null) {
+    return reduce(genFn, (item) => [pfx.concat(item[0]), item[1]])
+  }
+  else {
+    return reduce(function*() {
+      for (let i=0; i<count; ++i) {
+        let p = px === null ? null : px * i
+        yield prefix(pfx.concat([i]), {}, offset({ b: bx * i, p: p }, genFn))
       }
-      return opts
-    }
-  
-    static offset({b = 0, p = 0} = {}, genFn) {
-      const self = ParamOptions
-      return self.reduce(genFn, (item) => {
-        if (item[1].b !== null) {
-          item[1].b += b
-        }
-        if (item[1].p !== null) {
-          item[1].p += p
-        }          
-        return item
-      })
-    }
-    
-  
-    static inc({b = null, p = null} = {}, genFn) {
-      let opts = []
-      for(let arr of genFn()) {
-        for (let i = 0; i < arr.length; ++i) {
-          if (b !== null) {
-            arr[i][1].b = b + i
-          }
-          if (p !== null) {
-            arr[i][1].p = p + i
-          }
-          opts.push(arr[i]);
-        }
-      }
-      return opts
-    }
-  
-  //   /// Transform array of ParamOptions to dictionary. Later entries in the array with duplicate paths will overwrite earlier entries in the resulting dictionary.
-  //   static func paramsFromOpts(_ ins: [ParamOptions]) -> [SynthPath:Param] {
-  //     var dict = [SynthPath:Param]()
-  //     ins.forEach { po in
-  //       dict[po.path] = po.param()
-  //     }
-  //     return dict
-  //   }
-  //   
+    })
+  }
 }
 
-module.exports = ParamOptions
+function prefixes(pfxs, {bx = 0, px = null} = {}, genFn) {
+  let opts = []
+  for (let i=0; i<pfxs.length; ++i) {
+    const pfx = pfxs[i]
+    let p = px == null ? null : px * i
+    let item = prefix(pfx, {}, offset({ b: bx * i, p: p }, genFn))
+    opts = opts.concat(item)
+  }
+  return opts
+}
+
+function offset({b = 0, p = 0} = {}, genFn) {
+  return reduce(genFn, (item) => {
+    // ensure that we're not altering the data that's passed in.
+    const clone = item.slice()
+    clone[1] = Object.assign({}, clone[1])
+    if (clone[1].b !== null) {
+      clone[1].b += b
+    }
+    if (clone[1].p !== null) {
+      clone[1].p += p
+    }          
+    return clone
+  })
+}
+
+
+function inc({b = null, p = null} = {}, genFn) {
+  return reduce(genFn, (item, i) => {
+    const clone = item.slice()
+    clone[1] = Object.assign({}, clone[1])
+    if (b !== null) {
+      clone[1].b = b + i
+    }
+    if (p !== null) {
+      clone[1].p = p + i
+    }
+    return clone   
+  })
+}
+
+//   /// Transform array of ParamOptions to dictionary. Later entries in the array with duplicate paths will overwrite earlier entries in the resulting dictionary.
+//   static func paramsFromOpts(_ ins: [ParamOptions]) -> [SynthPath:Param] {
+//     var dict = [SynthPath:Param]()
+//     ins.forEach { po in
+//       dict[po.path] = po.param()
+//     }
+//     return dict
+//   }
+//   
+
+module.exports = {
+  reduce,
+  prefix,
+  prefixes,
+  offset,
+  inc,
+}
--- mopho/patch/voice.js
+++ mopho/patch/voice.js
@@ -1,228 +1,183 @@
 const { inc, prefix, prefixes } = require('/core/ParamOptions.js')
 require('/core/NumberUtils.js')
 
-function MophoTypePatch(idByte) {
-  return class {
-    static trussType = "SinglePatch"
-    static localType = "Patch"
-    
-    static fileDataCount = 298
-    static nameByteRange = [184, 200]
-    static sysexHeader = [0xf0, 0x01, idByte]
-    static bodyCount = 256
+module.exports = {
 
-    static expandedBodyCount = function() {
-      return this.fileDataCount - 5
-    }
+  mophoTypePatch: function(obj) {
+    const expandedBodyCount = obj.fileDataCount - 5
+    const bodyCount = obj.bodyCount
+    const idByte = obj.idByte
     
-    // static isValid(fileSize) {
-    //   return [fileDataCount, fileDataCount + 2].contains(fileSize)
-    // }
-    // 
-    static bytes = function(fileData) {
-      // make dependent on data count, since it can be 298 or 300 (300 is from bank)
-      const exp = this.expandedBodyCount()
-      const start = fileData.length - (exp + 1)
-      if (start < 0) {
-        return (this.bodyCount).map(() => 0)
-        // throw "fileData was too short: "+fileData.length
-      }
-      return Byte.unpack87(fileData, this.bodyCount, start, start + exp)
-    }
-    
-    
-    static sysexData = function(bytes, headerBytes) {
-      const packedBytes = Byte.pack78(bytes, this.expandedBodyCount())
-      return this.sysexHeader.concat(headerBytes).concat(packedBytes).concat([0xf7])
+    function sysexData(bytes, headerBytes) {
+      const packedBytes = Byte.pack78(bytes, expandedBodyCount)
+      return ([0xf0, 0x01, idByte]).concat(headerBytes).concat(packedBytes).concat([0xf7])
       // return .sysex(data)
     }
-    
-    static sysexWriteData = function(bytes, bank, location) {
-      return this.sysexData(bytes, [0x02, bank, location])
-    }
-    
-    static fileData = function(bytes) {
-      return this.sysexData(bytes, [0x03])
-    }
-    
-    // static func tamedRandomVoice() -> [SynthPath:Int] {
-    //   return [
-    //     [.amp, .level] : 0,
-    //     [.volume] : 127,
-    //     [.amp, .env, .delay] : 0,
-    //     [.amp, .env, .amt] : 127,
-    //   ]
-    // }
-    // 
+  
+    return Object.assign(obj, {
+      trussType: "SinglePatch",
+      localType: "Patch",
       
-    static osc = function*() {
-      yield prefix(["osc"], { count: 2, bx: 6, px: 5 }, function*() {
-        yield inc({ b: 0, p: 0 }, function*() { 
-          yield [
-            [["semitone"], { max: 120, isoS: Jiso.noteName("C0") }],
-            [["detune"], { max: 100, dispOff: -50 }],
-            [["shape"], { max: 103 }],
-            [["glide"], { }],
-            [["keyTrk"], { max: 1 }],
-          ] 
-        })
-      })
-      yield prefix(["osc"], { count: 2, bx: 6, px: 1 }, function*() {
-        yield [
-          [["sub"], { b: 5, p: 114 }],
-        ]
-      })
-    }
+      nameByteRange: [184, 200],
+      
+      // isValid(fileSize) {
+      //   return [fileDataCount, fileDataCount + 2].contains(fileSize)
+      // }
+      // 
+      bytes: function(fileData) {
+        // make dependent on data count, since it can be 298 or 300 (300 is from bank)
+        const exp = expandedBodyCount
+        const start = fileData.length - (exp + 1)
+        if (start < 0) {
+          return (bodyCount).map(() => 0)
+          // throw "fileData was too short: "+fileData.length
+        }
+        return Byte.unpack87(fileData, bodyCount, start, start + exp)
+      },
+      
+      sysexData: (bytes, headerBytes) => sysexData(bytes, headerBytes),
+      sysexWriteData: (bytes, bank, location) => sysexData(bytes, [0x02, bank, location]),
+      fileData: (bytes) => sysexData(bytes, [0x03]),
+      
+      // func tamedRandomVoice() -> [SynthPath:Int] {
+      //   return [
+      //     [.amp, .level] : 0,
+      //     [.volume] : 127,
+      //     [.amp, .env, .delay] : 0,
+      //     [.amp, .env, .amt] : 127,
+      //   ]
+      // }
+      // 
+    })
+  },
     
-    static filter = function*() {
-      yield inc({b: 20, p: 15}, function*() {
-        yield [
-          [["cutoff"], {max: 164}],
-          [["reson"], {}],
-        ]
-        yield prefix(["filter"], {}, function*() {
-          yield [
-            [["keyTrk"], {}],
-            [["extAudio"], {}],
-            [["fourPole"], {max: 1}],
-          ]
-          yield prefix(["env"], {}, function*() {
-            yield [
-              [["amt"], {max: 254, dispOff: -127}],
-              [["velo"], {}],
-              [["delay"], {}],
-              [["attack"], {}],
-              [["decay"], {}],
-              [["sustain"], {}],
-              [["release"], {}],
-            ]
-          })
-        })
-      })
-    }
-    
-    static ampEnv = function*() {
+  osc: function*() {
+    yield prefix(["osc"], { count: 2, bx: 6, px: 5 },
+      inc({ b: 0, p: 0 }, [
+        [["semitone"], { max: 120, isoS: Jiso.noteName("C0") }],
+        [["detune"], { max: 100, dispOff: -50 }],
+        [["shape"], { max: 103 }],
+        [["glide"], { }],
+        [["keyTrk"], { max: 1 }],
+      ])
+    )
+    yield prefix(["osc"], { count: 2, bx: 6, px: 1 }, [
+      [["sub"], { b: 5, p: 114 }],
+    ])
+  },
+  
+  filter: inc({b: 20, p: 15}, function*() {
+    yield [
+      [["cutoff"], {max: 164}],
+      [["reson"], {}],
+    ]
+    yield prefix(["filter"], {}, function*() {
       yield [
-        [["amp", "level"], {b: 32, p: 27}],
+        [["keyTrk"], {}],
+        [["extAudio"], {}],
+        [["fourPole"], {max: 1}],
       ]
-      yield prefix(["amp", "env"], {}, function*() {
-        yield inc({b: 33, p: 30}, function*() { 
-          yield [
-            [["amt"], {}],
-            [["velo"], {}],
-            [["delay"], {}],
-            [["attack"], {}],
-            [["decay"], {}],
-            [["sustain"], {}],
-            [["release"], {}],
-          ]
-        })
-      })
-    }
-    
-    static lfo = function*(b, self) {
-      yield prefix(["lfo"], {count: 4, bx: 5, px: 5}, function*() {
-        yield inc({b: b, p: 37}, function*() { 
-          yield [
-            [["freq"], {max: 166}],
-            [["shape"], {opts: self.lfoWaveOptions}],
-            [["amt"], {}],
-            [["dest"], {opts: self.modDestOptions}],
-            [["key", "sync"], {max: 1}],
-          ] 
-        })
-      })
-    }
-    
-    static env3 = function*(b, repB, self) {
-      yield prefix(["env", 2], {}, function *() {
-        yield inc({b: b, p: 57}, function *() {
-          yield [
-            [["dest"], {opts: self.modDestOptions}],
-            [["amt"], {max: 254, dispOff: -127}],
-            [["velo"], {}],
-            [["delay"], {}],
-            [["attack"], {}],
-            [["decay"], {}],
-            [["sustain"], {}],
-            [["release"], {}],
-          ]
-        })
-        yield [
-          [["rrepeat"], {b: repB, p: 98, max: 1}],
-        ]
-      })
-    }
-    
-    static mods = function*(b, self) {
-      yield prefix(["mod"], {count: 4, bx: 3, px: 3}, function*() {
-        yield inc({b: b, p: 65}, function*() {
-          yield [
-            [["src"], {opts: self.modSrcOptions}],
-            [["amt"], {max: 254, dispOff: -127}],
-            [["dest"], {opts: self.modDestOptions}],
-          ]
-        })
-      })
-    }
-    
-    static ctrls = function*(b, self) {
-      yield prefixes([["modWheel"], ["pressure"], ["breath"], ["velo"], ["foot"]], {bx: 2, px: 2}, function*() {
-        yield inc({b: b, p: 81}, function*() {
-          yield [
-            [["amt"], {max: 254, dispOff: -127}],
-            [["dest"], {opts: self.modDestOptions}],
-          ]
-        })
-      })
-    }
-    
-    static pushIt = function*(b, self) {
-      yield prefix(["pushIt"], {}, function*() {
-        yield inc({b: b, p: 111}, function*() {
-          yield [
-            [["note"], {max: 120, isoS: Jiso.noteName("C0")}],
-            [["velo"], {}],
-            [["mode"], {opts: self.pushItModeOptions}],
-          ]
-        })
-      })
-    }
-    
-    static tempoArpSeq = function*(b, self) {
-      yield inc({b: b}, function*() {
-        yield [
-          [["tempo"], {p: 91, min: 30, max: 250}],
-          [["clock", "divide"], {p: 92, opts: self.clockDivOptions}],
-          [["arp", "mode"], {p: 97, opts: self.arpModeOptions}],
-          [["arp", "on"], {p: 100, max: 1}],
-          [["seq", "trigger"], {p: 94, opts: self.seqTrigOptions}],
-          [["seq", "on"], {p: 101, max: 1}],
-        ]
-      })
-    }
-    
-    static seqSteps = function*() {
-      yield prefix(["seq"], {count: 4, bx: 16, px: 16}, function*() {
-        yield prefix(["step"], {count: 16, bx: 1, px: 1}, function*() {
-          yield [
-            [[], {b: 120, p: 120}],
-          ]
-        })
-      })
-    }
-    
-    // static func unison(b: Int) -> [ParamOptions] {
-    //   inc(b: b) {[
-    //     [["unison", "mode"], p: 95, opts: unisonModeOptions), // NEW
-    //     [["keyAssign"], p: 96, opts: keyAssignOptions),
-    //     [["unison"], p: 99, max: 1), // NEW
-    //   ]}
-    // 
-    // }
-
-  }
-}
-
-
-module.exports = MophoTypePatch 
+      yield prefix(["env"], {}, [
+        [["amt"], {max: 254, dispOff: -127}],
+        [["velo"], {}],
+        [["delay"], {}],
+        [["attack"], {}],
+        [["decay"], {}],
+        [["sustain"], {}],
+        [["release"], {}],
+      ])
+    })
+  }),
+  
+  ampEnv: function*() {
+    yield [
+      [["amp", "level"], {b: 32, p: 27}],
+    ]
+    yield prefix(["amp", "env"], {},
+      inc({b: 33, p: 30}, [
+        [["amt"], {}],
+        [["velo"], {}],
+        [["delay"], {}],
+        [["attack"], {}],
+        [["decay"], {}],
+        [["sustain"], {}],
+        [["release"], {}],
+      ])
+    )
+  },
+  
+  lfo: (b, obj) => prefix(["lfo"], {count: 4, bx: 5, px: 5},
+    inc({b: b, p: 37}, [
+      [["freq"], {max: 166}],
+      [["shape"], {opts: obj.lfoWaveOptions}],
+      [["amt"], {}],
+      [["dest"], {opts: obj.modDestOptions}],
+      [["key", "sync"], {max: 1}],
+    ])
+  ),
+  
+  env3: (b, repB, obj) => prefix(["env", 2], {}, function *() {
+    yield inc({b: b, p: 57}, [
+      [["dest"], {opts: obj.modDestOptions}],
+      [["amt"], {max: 254, dispOff: -127}],
+      [["velo"], {}],
+      [["delay"], {}],
+      [["attack"], {}],
+      [["decay"], {}],
+      [["sustain"], {}],
+      [["release"], {}],
+    ])
+    yield [
+      [["rrepeat"], {b: repB, p: 98, max: 1}],
+    ]
+  }),
+  
+  mods: (b, obj) => prefix(["mod"], {count: 4, bx: 3, px: 3},
+    inc({b: b, p: 65}, [
+      [["src"], {opts: obj.modSrcOptions}],
+      [["amt"], {max: 254, dispOff: -127}],
+      [["dest"], {opts: obj.modDestOptions}],
+    ])
+  ),
+  
+  ctrls: (b, obj) => prefixes([["modWheel"], ["pressure"], ["breath"], ["velo"], ["foot"]], {bx: 2, px: 2},
+    inc({b: b, p: 81}, [
+      [["amt"], {max: 254, dispOff: -127}],
+      [["dest"], {opts: obj.modDestOptions}],
+    ])
+  ),
+  
+  pushIt: (b, obj) => prefix(["pushIt"], {},
+    inc({b: b, p: 111}, [
+      [["note"], {max: 120, isoS: Jiso.noteName("C0")}],
+      [["velo"], {}],
+      [["mode"], {opts: obj.pushItModeOptions}],
+    ])
+  ),
+  
+  tempoArpSeq: function(b, obj) {
+    return inc({b: b}, [
+      [["tempo"], {p: 91, min: 30, max: 250}],
+      [["clock", "divide"], {p: 92, opts: obj.clockDivOptions}],
+      [["arp", "mode"], {p: 97, opts: obj.arpModeOptions}],
+      [["arp", "on"], {p: 100, max: 1}],
+      [["seq", "trigger"], {p: 94, opts: obj.seqTrigOptions}],
+      [["seq", "on"], {p: 101, max: 1}],
+    ])
+  },
+  
+  seqSteps: prefix(["seq"], {count: 4, bx: 16, px: 16}, 
+    prefix(["step"], {count: 16, bx: 1, px: 1}, [
+      [[], {b: 120, p: 120}],
+    ])
+  ),
+  
+  // func unison(b: Int) -> [ParamOptions] {
+  //   inc(b: b) {[
+  //     [["unison", "mode"], p: 95, opts: unisonModeOptions), // NEW
+  //     [["keyAssign"], p: 96, opts: keyAssignOptions),
+  //     [["unison"], p: 99, max: 1), // NEW
+  //   ]}
+  // 
+  // }
+} 
