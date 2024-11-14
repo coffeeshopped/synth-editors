daf894ff7c27b220a78606c3c895805c721bb1bb
Chadwick Wood <chadwick@coffeeshopped.com>
2024-11-14 09:57:20 -0600

    More Blofeld VoiceController

--- blofeld/BlofeldVoiceController.js
+++ blofeld/BlofeldVoiceController.js
@@ -1,23 +1,23 @@
 
 
-const modCombo = (label, pre) => ({
-  items: [
-    [label, [pre, "amt"]],
-    `← ${label} Src`, [pre, "src"]],
-  ],
-  cmd: ['patchChange', {
-    paths: [pre + "amt", pre + "src"], 
-    fn: values => {
-      guard let src = values[pre + "src"],
-            let amt = values[pre + "amt"] else { return [] }
-      let off = src == 0 || amt == 64
-      return [
-        ['dimItem', off, pre + "amt"],
-        ['dimItem', off, pre + "src"],
-      ]
-    }
-  }],
-})
+const modItems = (label, pre) => [
+  [label, [pre, "amt"]],
+  `← ${label} Src`, [pre, "src"]],
+]
+
+const modEffect = pre => ['patchChange', {
+  paths: [pre + "amt", pre + "src"], 
+  fn: values => {
+    const src = values[pre + "src"] || 0
+    const amt = values[pre + "amt"] || 0
+    const off = src == 0 || amt == 64
+    return [
+      ['dimItem', off, pre + "amt"],
+      ['dimItem', off, pre + "src"],
+    ]
+  }
+}]
+
 
 const fmCombo = {
   items: [
@@ -32,7 +32,6 @@ const fmCombo = {
 
 
 const oscController = index => {
-  const pwm = modCombo("PWM", "pw")
   var dim = ['dimsOn', {paths: ["shape", "sample"]}]
   var items = [
     [{switch: "Mode"}, "sample"],
@@ -56,7 +55,7 @@ const oscController = index => {
         ["Detune", "fine"],
         ["PW", "pw"],
       ].concat(
-        pwm.items
+        modItems("PWM", "pw")
       ).concat(
         fmCombo.items
       ).concat([
@@ -71,199 +70,200 @@ const oscController = index => {
       dim,
       ['patchChange', "sample", v => ['configCtrl', "shape", {opts:  $0 == 0 ? waveformOptions : sampleOptions}]],
       fm.cmd,
+      modEffect('pw'),
     ],
   }
 }
 
-const filterController = {
-  let cutoff = modCombo("Cutoff Mod", "cutoff")
-  let fm = fmCombo()
-  let pan = modCombo("Pan Mod", "pan")
-
-  return ['index', "filter", "type", i => { "Filter \($0+1)" }, [
+const filterController = ['index', "filter", "type", i => `Filter ${i + 1}`, {
+  builders: [
     ['grid', [[
       [{select: "Filter"}, "type"],
       ["cutoff"],
       ["reson"],
       ["Env Amt", "env/amt"],
       ["Velocity", "velo"],
-    ], cutoff.items + fm.items, [
+    ], modItems("Cutoff Mod", "cutoff").concat(fmCombo.items), ([
       ["pan"],
-    ] + pan.items + [
+    ]).concat(modItems("Pan Mod", "pan")).concat([
       ["drive"],
       ["Key Track", "keyTrk"],
-    ]]],
-  ], effects: [
-    cutoff.cmd,
-    fm.cmd,
-    pan.cmd,
+    ])]],
+  ], 
+  effects: [
+    modEffect("cutoff"),
+    fmCombo.cmd,
+    modEffect("pan"),
     ['dimsOn', "type"]
-  ])
-}
+  ],
+}]
 
-const envItem = (_ label: String?, prefix: SynthPath = [], _ id: SynthPath) => {
-  let pathFn: PatchController.DisplayPathFn = { path, values in
-    let mode = EnvelopeMode(rawValue: Int(values["mode"] ?? 0)) ?? .ADS1DS2R
-    let attackLevel = values["attack/level"] ?? 0
-    let attack = values["attack"] ?? 0
-    let decay = values["decay"] ?? 0
-    let sustain = values["sustain"] ?? 0
-    let decay2 = values["decay2"] ?? 0
-    let sustain2 = values["sustain2"] ?? 0
-    let rrelease = values["release"] ?? 0
 
-    let adsrStyle = mode == .ADSR || mode == .OneShot
-    let segCount: CGFloat = {
-      switch mode {
-      case .ADSR, .LoopAll, .LoopS1S2: return 4
-      case .ADS1DS2R: return 5
-      case .OneShot: return 3
-      }
-    }()
-    let segWidth = 1 / segCount
-    var x: CGFloat = 0
-    
-    path.move(to: CGPoint(x: 0, y: 0))
-    
-    // attack
-    x += attack * segWidth
-    path.addLine(to: CGPoint(x: x, y: adsrStyle ? 1 : attackLevel))
-    
-    // decay
-    x += decay * segWidth
-    path.addCurve(to: CGPoint(x: x, y: sustain))
+const pathFn = values => {
+  const mode = values["mode"] || 0
+  const attackLevel = values["attack/level"] || 0
+  const attack = values["attack"] || 0
+  const decay = values["decay"] || 0
+  const sustain = values["sustain"] || 0
+  const decay2 = values["decay2"] || 0
+  const sustain2 = values["sustain2"] || 0
+  const rrelease = values["release"] || 0
 
-    if mode == .ADSR {
-      // sustain1
-      x += segWidth
-      path.addLine(to: CGPoint(x: x, y: sustain))
+  var cmds = []
+
+  const adsrStyle = mode == 0 || mode == 2
+  const segCount = (() => {
+    switch (mode) {
+    case 0:
+    case 3:
+    case 4: 
+      return 4
+    case 1: 
+      return 5
+    case 2: 
+      return 3
     }
-    
-    if !adsrStyle {
-      // s1 to s2
-      x += decay2 * segWidth
-      path.addLine(to: CGPoint(x: x, y: sustain2))
-    }
-    
-    if mode == .ADS1DS2R {
-      // sustain2
-      x += segWidth
-      path.addLine(to: CGPoint(x: x, y: sustain2))
-    }
-    
-    // release
-    x += rrelease * segWidth
-    let rWeight = mode == .OneShot ? 0.5 : 0
-    path.addCurve(to: CGPoint(x: x, y: 0), weight: rWeight)      
+  })()
+  const segWidth = 1 / segCount
+  var x = 0
+  
+  cmds.push(['move', 0, 0])
+  
+  // attack
+  x += attack * segWidth
+  cmds.push(['line', x, adsrStyle ? 1 : attackLevel])
+  
+  // decay
+  x += decay * segWidth
+  cmds.push(['curve', x, sustain])
+
+  if (mode == 0) {
+    // sustain1
+    x += segWidth
+    cmds.push(['line', x, sustain])
   }
   
-  let maps: [PatchController.DisplayMap] = [
-    .ident("mode"),
-    .unit("attack/level"),
-    .unit("attack"),
-    .unit("decay"),
-    .unit("sustain"),
-    .unit("decay2"),
-    .unit("sustain2"),
-    .unit("release"),
+  if (!adsrStyle) {
+    // s1 to s2
+    x += decay2 * segWidth
+    cmds.push(['line', x, sustain2])
+  }
+  
+  if (mode == 1) {
+    // sustain2
+    x += segWidth
+    cmds.push(['line', x, sustain2])
+  }
+  
+  // release
+  x += rrelease * segWidth
+  const rWeight = mode == 2 ? 0.5 : 0
+  cmds.push(['curve', x, 0, rWeight])
+  
+  return cmds
+}
+
+const envItem = (label, prefix, id) => ({
+  env: pathFn,
+  l: label,
+  maps: [
+    ['=', "mode"],
+    ['u', "attack/level"],
+    ['u', "attack"],
+    ['u', "decay"],
+    ['u', "sustain"],
+    ['u', "decay2"],
+    ['u', "sustain2"],
+    ['u', "release"],
+  ],
+  srcPrefix: prefix,
+  id: id,
+})
+
+const envMenu = (prefix, id) => ['editMenu', id, {
+  paths: (["mode", "attack", "attack/level", "decay", "sustain", "decay2", "sustain2", "release"]).map(p => [prefix, p]), 
+  type: "MicroQEnvelope", 
+  init: [0, 0, 64, 0, 127, 64, 64, 0], 
+  // rand: { [5.rand()] + 7.map { 128.rand() } },
+}]
+
+const envEffect = prefix => ['patchChange', [prefix, "mode"], v => {
+  const hideSome = (v == 0) || (v == 2) // 0,2==adsr
+  return [
+    ['setCtrlLabel', "decay", hideSome ? "D" : "D1"],
+    ['setCtrlLabel', "sustain", hideSome ? "S" : "S1"],
+    ['hideItem', hideSome, "attack/level"],
+    ['hideItem', hideSome, "decay2"],
+    ['hideItem', hideSome, "sustain2"],
   ]
-  
-  let item: PatchController.PanelItem = .display(.env(pathFn), label, maps.map { $0.srcPrefix(prefix) }, id: id)
-  
-  let paths: [SynthPath] = ["mode", "attack", "attack/level", "decay", "sustain", "decay2", "sustain2", "release"].map { prefix + $0 }
-  let cmd: PatchController.Effect = ['editMenu', id, paths: paths, type: "MicroQEnvelope", init: [0, 0, 64, 0, 127, 64, 64, 0], rand: { [5.rand()] + 7.map { 128.rand() } }]
-  
-  return (item, cmd)
-}
+}]
 
-const envController = (withFilter: Bool = false) => {
+
+const envController = withFilter => {
+  const items = (withFilter ? ["Filter"] : []).concat(["Amp","3","4"])
+  const labels = (withFilter ? ["Filter"] : []).concat(["Amp", "Env 3", "Env 4"])
   
-  let items = (withFilter ? ["Filter"] : []) + ["Amp","3","4"]
-  let knobs = envControllerSetup()
-      
-  let labels = (withFilter ? ["Filter"] : []) + ["Amp", "Env 3", "Env 4"]
-  let env = envItem(nil, "env")
-  return .patch(prefix: .indexFn({ "env/$0 + (withFilter ? 0 : 1)" }), [
-    .grid([
-      knobs.items, [
-        ['switcher', label: "Envelope", items, width: withFilter ? 7 : 5],
+  return {
+    prefix: ['indexFn', i => `env/${i + (withFilter ? 0 : 1)}`, 
+    builders: [
+      ['grid', [[
+        ["AL", "attack/level"], // 0
+        ["A", "attack"], // 1
+        ["D1", "decay"], // 2
+        ["S1", "sustain"], // 3
+        ["D2", "decay2"], // 4
+        ["S2", "sustain2"], // 5
+        ["R", "release"], // 6
+      ],[
+        ['switcher', "Envelope", items, {width: withFilter ? 7 : 5}],
         [{select: "Mode"}, "mode"],
-        env.0,
-        [{switsch: "Trigger"}, "trigger"],
-    ]]],
-  ], effects: [
-    knobs.cmd,
-    env.1,
-    .indexChange({
-      [['setCtrlLabel', "env", labels[$0 % labels.count]]]
-    })
-  ])
-
-}
-
-const envControllerSetup = (prefix: SynthPath = []) => {
-  return (
-    items: [
-      ["AL", "attack/level"], // 0
-      ["A", "attack"], // 1
-      ["D1", "decay"], // 2
-      ["S1", "sustain"], // 3
-      ["D2", "decay2"], // 4
-      ["S2", "sustain2"], // 5
-      ["R", "release"], // 6
+        envItem(null, '', "env"),
+        [{switch: "Trigger"}, "trigger"],
+      ]]],
+    ], 
+    effects: [
+      envEffect(''),
+      envMenu('', 'env'),
+      ['indexChange', i => ['setCtrlLabel', "env", labels[i % labels.length]]],
     ],
-    cmd: .patchChange(prefix + "mode", {
-      let hideSome = [0, 2].contains($0) // 0,2==adsr
-      return [
-        ['setCtrlLabel', "decay", hideSome ? "D" : "D1"],
-        ['setCtrlLabel', "sustain", hideSome ? "S" : "S1"],
-        ['dimItem', hideSome, "attack/level", dimAlpha: 0],
-        ['dimItem', hideSome, "decay2", dimAlpha: 0],
-        ['dimItem', hideSome, "sustain2", dimAlpha: 0],
-      ]
-    })
-  )
+  }
 }
 
-static let lfoController: PatchController = .patch(prefix: .index("lfo"), [
-  ['grid', [[
-    ["speed"],
-    [{checkbox: "Clocked"}, "clock"],
-    ["Key Track", "keyTrk"],
-    ["phase"],
-    ["delay"],
-    ["fade"],
-  ],[
-    ['switcher', label: "LFO", ["1","2","3"]],
-    [{select: "Wave"}, "shape"],
-    "sync",
-  ]]],
-], effects: [
-  .indexChange({
-    [['setCtrlLabel', "shape", "LFO \($0 + 1)"]]
-  }),
-  .patchChange("clock", {
-    [['configCtrl', "speed", .opts(ParamOptions(isoS: $0 == 0 ? nil :lfoRateIso))]]
-  }),
-  ['dimsOn', "sync", id: "phase"]
-])
+
+const lfoController = {
+  prefix: .index("lfo"), 
+  builders: [
+    ['grid', [[
+      ["speed"],
+      [{checkbox: "Clocked"}, "clock"],
+      ["Key Track", "keyTrk"],
+      ["phase"],
+      ["delay"],
+      ["fade"],
+    ],[
+      ['switcher', label: "LFO", ["1","2","3"]],
+      [{select: "Wave"}, "shape"],
+      "sync",
+    ]]],
+  ], 
+  effects: [
+    ['indexChange', i => ['setCtrlLabel', "shape", `LFO ${i + 1}`]],
+    ['patchChange', "clock", v => ['configCtrl', "speed", { iso: v == 0 ? null : lfoRateIso }],
+    ['dimsOn', "sync", "phase"],
+  ],
+}
 
 
 const voiceController = {
-  let pitch = modCombo("Pitch Mod", "pitch")
-  
-  let knobs = envControllerSetup(prefix: "env/0")
-
-  let env = envItem("Filter", prefix: "env/0", "env/0")
   builders: [
     // add before fEnv panels so it's in the back.
     ['panel', 'fEnvCon', { color: 2, }, [[]]],
-    ['child', oscController(0), "osc0", color: 1],
-    ['child', oscController(1), "osc1", color: 1],
-    ['child', oscController(2), "osc2", color: 1],
-    ['children', 2, "filter", color: 2, filterController],
-    ['child', envController(), "env", color: 3],
-    ['child', lfoController, "lfo", color: 3],
+    ['child', oscController(0), "osc0", {color: 1}],
+    ['child', oscController(1), "osc1", {color: 1}],
+    ['child', oscController(2), "osc2", {color: 1}],
+    ['children', 2, "filter", {color: 2}, filterController],
+    ['child', envController(false), "env", {color: 3}],
+    ['child', lfoController, "lfo", {color: 3}],
     ['panel', 'mix', { color: 1, }, [[
       ["O1 Lvl", "osc/0/level"],
       ["O1 Bal", "osc/0/balance"],
@@ -283,30 +283,31 @@ const voiceController = {
     ['panel', 'noise', { color: 1, }, [[
       ["Nz Color", "noise/color"],
     ]]],
-    .panel("pitch", color: 1, [pitch.items]),
+    ['panel', "pitch", {color: 1}, [modItems("Pitch Mod", "pitch")]),
     ['panel', 'route', { color: 2, }, [[
       [{switsch: "Routing"}, "filter/routing"],
     ]]],
     ['panel', 'fEnv', { prefix: "env/0", color: 2, }, [[
-      knobs.items[1],
-      knobs.items[2],
-      knobs.items[3],
+      ["A", "attack"], // 1
+      ["D1", "decay"], // 2
+      ["S1", "sustain"], // 3
     ],[
-      knobs.items[0],
-      [{switsch: "Trigger"}, "trigger"],
-      knobs.items[6],
+      ["AL", "attack/level"], // 0
+      [{switch: "Trigger"}, "trigger"],
+      ["R", "release"], // 6
     ],[
       [{select: "Mode"}, "mode"],
-      env.0,
+      envItem("Filter", "env/0", "env/0"),
     ]]],
     ['panel', 'fEnvX', { prefix: "env/0", color: 2, }, [[
-      knobs.items[4],
-      knobs.items[5],
+      ["D2", "decay2"], // 4
+      ["S2", "sustain2"], // 5
     ]]],
   ], 
   effects: [
-    knobs.cmd,
-    env.1,
+    envEffect("env/0"),
+    envMenu("env/0", "env/0"),
+    modEffect("pitch"),
   ], 
   layout: [
     ['row', [["mix", 2], ["osc0", 16]], { opts: ['alignAllTop'] }],
@@ -330,8 +331,8 @@ const voiceController = {
 }
 
 
-
-static let modsController: PatchController = .patch([
+const modsController = {
+  builders: [
   .children(16, "mod", color: 1, {
     .patch(prefix: .index("mod"), [
       ['grid', [[
@@ -375,21 +376,21 @@ static let modsController: PatchController = .patch([
 
 
 static var fxController: PatchController {
-  let knobCount = 14
+  const knobCount = 14
   
-  let setKnobValues: PatchController.Effect = .patchChange(paths: ["type"] + 14.map { "param/$0" }, { values in
-    guard let fxType = values["type"] else { return [] }
+  const setKnobValues: PatchController.Effect = .patchChange(paths: ["type"] + 14.map { "param/$0" }, { values in
+    guard const fxType = values["type"] else { return [] }
     return knobCount.compactMap {
-      guard let pindex = paramIndex(forType: fxType, knob: $0) else { return nil }
+      guard const pindex = paramIndex(forType: fxType, knob: $0) else { return nil }
       return ['setValue', "$0", values["param/pindex"] ?? 0]
     }
   })
   
   // configure knobs when fxType changes
-  let knobConfig: PatchController.Effect = .patchChange("type", { v in
-    guard let fxMap = fxParams(forType: v) else { return [] }
+  const knobConfig: PatchController.Effect = .patchChange("type", { v in
+    guard const fxMap = fxParams(forType: v) else { return [] }
     return knobCount.flatMap {
-      let knob: SynthPath = "$0"
+      const knob: SynthPath = "$0"
       return [
         ['dimItem', $0 >= fxMap.count, knob, dimAlpha: 0]
       ] + ($0 >= fxMap.count ? [] : [
@@ -399,11 +400,11 @@ static var fxController: PatchController {
     }
   })
 
-  let knobControlChanges: [PatchController.Effect] = knobCount.map { k in
+  const knobControlChanges: [PatchController.Effect] = knobCount.map { k in
       .controlChange("k") { state, locals in
-        guard let fxType = state.prefixedValue("type"),
-              let pindex = paramIndex(forType: fxType, knob: k),
-              let v = locals["k"] else { return nil }
+        guard const fxType = state.prefixedValue("type"),
+              const pindex = paramIndex(forType: fxType, knob: k),
+              const v = locals["k"] else { return nil }
         return .paramsChange(["param/pindex" : v])
       }
     }
@@ -427,13 +428,13 @@ const fxParams = (forType type: Int) => {
 }
 
 const paramIndex = (forType type: Int, knob: Int) => {
-  guard let fxMap = fxParams(forType: type),
+  guard const fxMap = fxParams(forType: type),
         knob < fxMap.count else { return nil }
   return (fxMap[knob].path.i(0) ?? 146) - 146
 }
 
 
-static let arp: PatchController = .patch([
+static const arp: PatchController = .patch([
   ['children', 2, "fx", fxController],
   ['panel', 'cat', { color: 1, }, [[
     [{select: "Category"}, "category"],
@@ -460,7 +461,7 @@ static let arp: PatchController = .patch([
   arpStepPanel("glide", "Glide", .glide, .checkbox),
 ], effects: [
   .patchChange("arp/pattern/length", { v in
-    let pathItems: [SynthPathItem] = "step/length/timing/accent/glide"
+    const pathItems: [SynthPathItem] = "step/length/timing/accent/glide"
     return 16.flatMap { step in
       pathItems.map {
         ['dimItem', step > v, `step/${$0}`, dimAlpha: 0.25]
@@ -485,12 +486,12 @@ static let arp: PatchController = .patch([
   ])
 ])
 
-static let stepCount = 16
+static const stepCount = 16
 
 const arpStepPanel = (_ panel: String, _ label: String, _ pathItem: SynthPathItem, _ control: PatchController.Control) => {
   .panel(panel, prefix: "arp", color: 2, [stepCount.map {
-    let label = $0 == 0 ? "\(label) 1" : "\($0 + 1)"
-    let path: SynthPath = `$0/${pathItem}`
+    const label = $0 == 0 ? "\(label) 1" : "\($0 + 1)"
+    const path: SynthPath = `$0/${pathItem}`
     switch control {
     case .checkbox:
       return [{checkbox: label}, path]
@@ -504,7 +505,7 @@ const arpStepPanel = (_ panel: String, _ label: String, _ pathItem: SynthPathIte
 
 module.exports = {
   controller: {
-    let ampCombo = modCombo("Amp Mod", "amp/mod")
+    const ampCombo = modCombo("Amp Mod", "amp/mod")
     return .paged([
       ['switcher', ["Main", "Mods", "FX/Arp"], color: 1],
       ['panel', 'glide', { prefix: "glide", color: 1, }, [[
